7f4478144406ab1447b96c4f8beb955a44fba4c2
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2018-04-22 12:11:49 +0200

    Add files via upload

---
+++ Kernel/Digits.h
@@ -0,0 +1,10 @@
+//Copyright 2018 Iris Technologies, All Rights Reserved
+#pragma once
+#include <Iris\Types.h>
+#include <Iris\Call.h>
+
+namespace Iris
+{
+	template<class T>
+	U8 Digits(const T);
+}
